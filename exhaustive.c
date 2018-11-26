7086e05e69c2cad2c10c1d3e28604880a0c17f6d
Bernhard Haubold <haubold@evolbio.mpg.de>
2018-11-26 16:50:18 +0100

    Improved Exhaustive Search
    Replaced Knuth's Algorithm L (TACP, Vol. 4A, Part 1, p. 358) by its
    faster sibling, Algorithm T (TACP, Vol, 4A, Part 1, p. 359). The run
    time improvement is approximately only 1/4 percent. For realistic sample
    sizes and more than a few levels the exhaustive search quickly becomes
    unfeasible.

--- exhaustive.c
+++ exhaustive.c
@@ -1,6 +1,9 @@
 /***** exhaustive.c *******************************
- * Description: Knuth's Algorithm L for finding all
- * combinations; Vol 4a of TACP.
+ * Description: Generate all combinations of 
+ * levels using Knuth's Algorithm T.
+ * Reference: Donald E. Knuth (2011). The Art of
+ *   Computer Programming. Volume 4A: Combinatorial
+ *   Algorithms, Part 1. Addison Wesley, p. 359.
  * Author: Bernhard Haubold, haubold@evolbio.mpg.de
  * Date: Fri Nov 23 16:18:33 2018
  **************************************************/
@@ -10,7 +13,7 @@
 /* se sets up the computation of all n \choose m combinations */
 static int *se(int m, int n) {
   int *c;
-
+  /* T1. [Initialize.] */
   c = (int *)emalloc((m + 3) * sizeof(int));
   for(int i = 1; i <= m; i++)
     c[i] = i - 1;
@@ -24,30 +27,50 @@ static int *se(int m, int n) {
 static int *ne(int m, int n, int *k, short setup) {
   static short finished = 0;
   static int *c;
+  static int j;
+  int x;
 
   if(setup) {
     finished = 0;
     c = se(m, n);
+    j = m;
     return NULL;
   }
   if(finished) {
     free(c);
     return NULL;
   }
+  /* T2. [Visit.] */
   k[1] = 2;
   for(int i = 1; i <= m; i++)
     k[i + 1] = c[i] + 3;
   k[m + 2] = n + 3;
-  int j = 1;
-  while(c[j] + 1 == c[j + 1]) {
-    c[j] = j - 1;
-    j++;
+  if(j > 0) {
+    x = j;
+  } else {
+    /* T3. [Easy case?] */
+    if(c[1] + 1 < c[2]) {
+      c[1]++;
+      return k;
+    } else {
+      j = 2;
+    }
+    /* T4. [Find j.] */
+    c[j - 1] = j - 2;
+    x = c[j] + 1;
+    while(x == c[j + 1]) {
+      j++;
+      c[j - 1] = j - 2;
+      x = c[j] + 1;
+    }
+    /* T5. [Done?] */
+    if(j > m) {
+      finished = 1;
+    }
   }
-  if(j > m) {
-    finished = 1;
-  }
-  if(!finished)
-    c[j]++;
+  /* T6. [Increase c_j.] */
+  c[j] = x;
+  j--;
 
   return k;
 }
