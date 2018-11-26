8f8be3bb0c376011a80e50aef36460ab87bf086a
Bernhard Haubold <haubold@evolbio.mpg.de>
2018-11-26 16:49:34 +0100

    Testing

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,37 +1,51 @@
-./epos -l 10000000 -u 1.2e-8 -U data/testNewtonU.dat > tmp.out
-DIFF=$(diff tmp.out data/testNewtonU.out)
+./epos -l 10000000 -u 1.2e-8 -U data/testU.dat > tmp.out
+DIFF=$(diff tmp.out data/testU.out)
 if [ "$DIFF" == "" ] 
 then
-    printf "Test(Newton, unfolded)\t\t\tpass\n"
+    printf "Test(unfolded, greedy)\t\tpass\n"
 else
-    printf "Test(Newton, unfolded)\t\t\tfail: %s\n" ${DIFF}
+    printf "Test(unfolded, greedy)\t\tfail\n"
+    echo ${DIFF}
 fi
 
-./epos -l 10000000 -u 1.2e-8 data/testNewtonF.dat > tmp.out
-DIFF=$(diff tmp.out data/testNewtonF.out)
+./epos -l 10000000 -u 1.2e-8 data/testF.dat > tmp.out
+DIFF=$(diff tmp.out data/testFg.out)
 if [ "$DIFF" == "" ] 
 then
-    printf "Test(Newton, folded)\t\t\tpass\n"
+    printf "Test(folded,   greedy)\t\tpass\n"
 else
-    printf "Test(Newton, folded)\t\t\tfail: %s\n" ${DIFF}
+    printf "Test(folded,   greedy)\t\tfail\n"
+    echo ${DIFF}
+fi
+
+./epos -E 10 -l 10000000 -u 1.2e-8 data/testF.dat > tmp.out
+DIFF=$(diff tmp.out data/testFe.out)
+if [ "$DIFF" == "" ] 
+then
+    printf "Test(folded,   exhaustive)\tpass\n"
+else
+    printf "Test(folded,   exhaustive)\tfail\n"
+    echo ${DIFF}
 fi
 
 ./epos data/kap144i.dat > tmp.out
 DIFF=$(diff tmp.out data/kap144i.out)
 if [ "$DIFF" == "" ] 
 then
-    printf "Test(Newton, kap144i, greedy)\t\tpass\n"
+    printf "Test(kap144i,  greedy)\t\tpass\n"
 else
-    printf "Test(Newton, kap144i, greedy)\t\tfail: %s\n" ${DIFF}
+    printf "Test(kap144i,  greedy)\t\tfail\n"
+    echo ${DIFF}
 fi
 
 ./epos -E 3 data/kap144i.dat > tmp.out
 DIFF=$(diff tmp.out data/kap144ie.out)
 if [ "$DIFF" == "" ] 
 then
-    printf "Test(Newton, kap144i, exhaustive)\tpass\n"
+    printf "Test(kap144i,  exhaustive)\tpass\n"
 else
-    printf "Test(Newton, kap144i, exhaustive)\tfail: %s\n" ${DIFF}
+    printf "Test(kap144i,  exhaustive)\tfail\n"
+    echo ${DIFF}
 fi
 
 rm tmp.out
