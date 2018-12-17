16184c58694315d996b45792f229763c8f3e2cca
Bernhard Haubold <haubold@evolbio.mpg.de>
2018-12-17 15:31:01 +0100

    Improved versioning from tags

--- scripts/getVersion.sh
+++ scripts/getVersion.sh
@@ -1,1 +1,2 @@
-git describe | sed -r 's/^[vV]//; s/-g(.......)/\\ \\(\1\\)\\/' | tr -d '\n'
+# git describe | sed -r 's/^[vV]//; s/-g(.......)/\\ \\(\1\\)\\/' | tr -d '\n'
+git describe | sed -r 's/^[vV]//; s/(.)$/\1\\/; s/-g(.......)/\\ \\(\1\\)/' | tr -d '\n'
