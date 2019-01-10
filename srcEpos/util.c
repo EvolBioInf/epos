28168955113c9c6a8e0babc1d35b726c63cf3d28
Bernhard Haubold <haubold@evolbio.mpg.de>
2019-01-10 17:15:12 +0100

    Small improvement of interface

--- srcEpos/util.c
+++ srcEpos/util.c
@@ -92,9 +92,9 @@ void printTimes(PopSizes *ps, Sfs *sfs){
 
   k = sfs->n;
   if(ps->l > 0)
-    printf("#Final        Log(Likelihood): %f\n", ps->l);
+    printf("#Final Log(Likelihood):          %f\n", ps->l);
   else
-    printf("#Final        Log(likelihood): 0.0\n");
+    printf("#Final Log(Likelihood):          %f\n", 0.0);
   printf("#Level\tT[Level]\tN[Level]\n");
   t = 0.;
   for(i=ps->m;i>=1;i--){
