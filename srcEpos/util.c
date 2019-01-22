5751c79ce529dab0f3b89456bccd3d6ee4704b9d
Bernhard Haubold <haubold@evolbio.mpg.de>
2019-01-22 11:18:53 +0100

    Formatting of d^2 and testing

--- srcEpos/util.c
+++ srcEpos/util.c
@@ -96,7 +96,7 @@ void printTimes(PopSizes *ps, Sfs *sfs){
     printf("#Final Log(Likelihood):          %f\n", ps->l);
   else
     printf("#Final Log(Likelihood):          %f\n", 0.0);
-  printf("#d^2:                              %f\n", d2);
+  printf("#d^2:                              %g\n", d2);
   printf("#Level\tT[Level]\tN[Level]\n");
   t = 0.;
   for(i=ps->m;i>=1;i--){
